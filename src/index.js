1a76f7212db4a7b3a634348b7ef8e24b77a76382
Kássia <kassia_guterres@outlook.com>
2022-08-27 12:55:17 -0300

    Get last 10 tweets without avatar

--- src/index.js
+++ src/index.js
@@ -1,52 +1,44 @@
 import express from "express";
 import cors from "cors";
-import { runInNewContext } from "vm";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 const tweets = [];
-const user = [];
-
-// GET /tweets
-server.get("/tweets", (request, response) => {
-  
-    response.send(tweets);
-   
-});
-
-
-
+const users = [];
 
 // POST /sign-up
 server.post("/sign-up", (request, response) => {
-     const newUser = request.body;
-    //console.log(newTweet);
-    
-    user.push(newUser);
-    
-    response.send("OK");
-  });
-  
+  const newUser = request.body;
+  //console.log(newUser);
+  users.push(newUser);
 
-// POST /tweets
-server.post("/tweets", (request, response) => {
-    
-  const newTweet = request.body;
-  //console.log(newTweet);
-  
-  tweets.push(newTweet);
-  
   response.send("OK");
 });
 
+// POST /tweets
+server.post("/tweets", (request, response) => {
+  const newTweet = request.body;
+  //console.log(newTweet);
+  tweets.push(newTweet);
+  response.send("OK");
+});
 
+// GET /tweets
+server.get("/tweets", (request, response) => {
+  const lastTweets = tweets.slice(-10); 
+  // [
+  //   {
+  //     username: "bobesponja",
+  //     avatar:
+  //       "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  //     tweet: "eu amo o hub",
+  //   },
+  // ];
 
-
-
-
-
+  response.send(lastTweets);
+});
 server.listen(5000, function () {
   console.log("Listening on 5000");
 });
