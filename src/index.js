dd29c467b1e7f7d7f7f09b28903fa57c9cbcdba6
Kássia <kassia_guterres@outlook.com>
2022-08-29 09:59:03 -0300

    Validation bonus

--- src/index.js
+++ src/index.js
@@ -10,40 +10,55 @@ const users = [];
 
 // POST /sign-up
 server.post("/sign-up", (request, response) => {
-  const newUser = request.body;
-  //console.log(newUser);
-  users.push(newUser);
-
-  response.send("OK");
+  // const newUser = request.body;
+  const { username, avatar } = request.body;
+  
+  if(!username || !avatar) {
+    return response.status(400).send("Todos os campos são obrigatórios!")
+  }
+  //users.push(newUser);
+  users.push({
+    username,
+    avatar,
+  })
+  //response.send("OK")
+  response.status(201).send("OK");
 });
 
 // POST /tweets
 server.post("/tweets", (request, response) => {
-  const newTweet = request.body;
-  //console.log(newTweet);
-  tweets.push(newTweet);
-  response.send("OK");
+  //const newTweet = request.body;
+  const { username, tweet } = request.body;
+  //tweets.push(newTweet);
+  //response.send("OK"); 
+  if(!username || !tweet) {
+    return response.status(400).send("Todos os campos são obrigatórios!")
+  }
+  tweets.push({
+    username,
+    tweet,
+  })
+  response.status(201).send("OK");
 });
 
 // GET /tweets
 server.get("/tweets", (request, response) => {
-  const lastTweets = tweets.slice(-10); 
+  const lastTweets = tweets.slice(-10);
   let usersTweet = [];
 
-  lastTweets.forEach(value => {
-    
+  lastTweets.forEach((value) => {
     let username = value.username;
     let tweet = value.tweet;
-    const user = users.find(value => value.username === username);
+    const user = users.find((value) => value.username === username);
     // Verifica se o usuário existe;
-    if (user !== undefined){
-    const post = {
-      ...user,
-      tweet,
-    } 
-    usersTweet.push(post);}
-   })
-
+    if (user !== undefined) {
+      const post = {
+        ...user,
+        tweet,
+      };
+      usersTweet.push(post);
+    }
+  });
 
   response.send(usersTweet);
 });
